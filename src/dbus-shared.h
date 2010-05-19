c4bdcc57819225a4ac52fa55d0578e30fef67b47
Ted Gould <ted@gould.cx>
2010-05-18 22:07:07 -0500

    Adding a shared version number

--- src/dbus-shared.h
+++ src/dbus-shared.h
@@ -2,6 +2,7 @@
 #define  SERVICE_NAME   "org.ayatana.indicator.datetime"
 #define  SERVICE_IFACE  "org.ayatana.indicator.datetime.service"
 #define  SERVICE_OBJ   "/org/ayatana/indicator/datetime/service"
+#define  SERVICE_VERSION  1
 
 #define  MENU_OBJ      "/org/ayatana/indicator/datetime/menu"
 
