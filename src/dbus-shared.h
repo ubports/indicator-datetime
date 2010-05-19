cfe8a541d7a9cd751240704fe94b431fd3d98b74
Ted Gould <ted@gould.cx>
2010-05-18 21:51:51 -0500

    Some shared names for our dbus stuff

---
+++ src/dbus-shared.h
@@ -0,0 +1,7 @@
+
+#define  SERVICE_NAME   "org.ayatana.indicator.datetime"
+#define  SERVICE_IFACE  "org.ayatana.indicator.datetime.service"
+#define  SERVICE_OBJ   "/org/ayatana/indicator/datetime/service"
+
+#define  MENU_OBJ      "/org/ayatana/indicator/datetime/menu"
+
