6e9a3d1612fd1318ab3d9e82306638c44b9bcf0b
Charles Kerr <charles.kerr@canonical.com>
2015-01-21 19:58:46 -0600

    in Snap's Sound() ctor, use an 'alarm' role for the alarm clock and 'alert' for calendar events

--- src/snap.cpp
+++ src/snap.cpp
@@ -95,10 +95,11 @@ public:
         std::shared_ptr<uin::Sound> sound;
         if (appointment.is_ubuntu_alarm() || !silent_mode()) {
             // create the sound.
+            const auto role = appointment.is_ubuntu_alarm() ? "alarm" : "alert";
             const auto uri = get_alarm_uri(appointment, m_settings);
             const auto volume = m_settings->alarm_volume.get();
             const bool loop = interactive;
-            sound = std::make_shared<uin::Sound>(uri, volume, loop);
+            sound = std::make_shared<uin::Sound>(role, uri, volume, loop);
         }
 
         // create the haptic feedback...
