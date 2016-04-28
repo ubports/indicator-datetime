f16fdde07ce0f4d6613af5633ed0155c6dbe17ea
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2016-04-28 11:56:11 -0300

    Detect desktop to launch applications.

--- src/main.cpp
+++ src/main.cpp
@@ -153,7 +153,7 @@ main(int /*argc*/, char** /*argv*/)
         snooze_planner->add(appointment, alarm);
     };
     auto on_ok = [actions](const Appointment& app, const Alarm&){
-        actions->phone_open_appointment(app, app.begin);
+        actions->open_appointment(app, app.begin);
     };
     auto on_alarm_reached = [&engine, &snap, &on_snooze, &on_ok](const Appointment& appointment, const Alarm& alarm) {
         (*snap)(appointment, alarm, on_snooze, on_ok);
