76eca9c404a782be272c8350e96864d44d6395d0
Rodney Dawes <dobey.pwns@gmail.com>
2018-10-31 21:16:15 -0400

    Fix clock app URL.

--- tests/test-live-actions.cpp
+++ tests/test-live-actions.cpp
@@ -161,7 +161,7 @@ TEST_F(TestLiveActionsFixture, DesktopOpenSettingsApp)
 
 namespace
 {
-    const std::string clock_app_url = "appid://ubuntu-clock-app/ubuntu-clock-app/current-user-version";
+    const std::string clock_app_url = "alarm://";
 }
 
 TEST_F(TestLiveActionsFixture, PhoneOpenAlarmApp)
