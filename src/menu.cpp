be8fe42b3599da6ee3abfda20114b196617159ff
kugiigi <kugi_eusebio@protonmail.com>
2021-03-20 21:29:06 +0800

    Check calendar selection 2

--- src/menu.cpp
+++ src/menu.cpp
@@ -486,7 +486,7 @@ private:
                 : calendar_day.start_of_day();
             
             //~ auto menu_item = g_menu_item_new (_("View Alarms…"), "indicator.phone.open-alarm-app");
-            auto menu_item = g_menu_item_new (begin.format(_("%A, %e %B %Y - %F %T"))), "indicator.phone.open-alarm-app");
+            auto menu_item = g_menu_item_new (begin.format("%A, %e %B %Y - %F %T"), "indicator.phone.open-alarm-app");
             g_menu_item_set_attribute_value (menu_item, G_MENU_ATTRIBUTE_ICON, get_serialized_alarm_icon());
             g_menu_append_item (menu, menu_item);
             g_object_unref (menu_item);
