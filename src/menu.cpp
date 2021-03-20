1ab477b1d61949263bdeccae22a23d013242f5ce
kugiigi <kugi_eusebio@protonmail.com>
2021-03-20 23:25:43 +0800

    Remove sorting

--- src/menu.cpp
+++ src/menu.cpp
@@ -115,7 +115,7 @@ Menu::get_display_appointments(const std::vector<Appointment>& appointments_in,
 
             return false;
         };
-        std::sort(appointments.begin(), appointments.end(), compare);
+        //~ std::sort(appointments.begin(), appointments.end(), compare);
         //~ appointments.resize(max_items);
     //~ }
 
@@ -137,7 +137,7 @@ Menu::get_display_appointments(const std::vector<Appointment>& appointments_in,
         return a.summary < b.summary;
     };
     //~ std::sort(appointments.begin(), appointments.end(), compare);
-    std::sort(appointments.begin(), appointments.end(), compare2);
+    //~ std::sort(appointments.begin(), appointments.end(), compare2);
     return appointments;
 }
 
